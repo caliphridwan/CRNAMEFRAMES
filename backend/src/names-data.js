3732004b9c4bb85c1b335a9261749ea5cd76dc13
caliphridwan <caliphridwan93@gmail.com>
2026-09-23 16:43:16 +0100

    Updated names

--- backend/src/names-data.js
+++ backend/src/names-data.js
@@ -73,6 +73,8 @@ const NAME_DB = [
   { name: "Bilqees", arabic: "بِلْقِيسُ", meaning: "Beautiful, Wealthy Queen " },
   { name: "Bilkis", arabic: "بِلْقِيسُ", meaning: "Beautiful, Wealthy Queen " },
   { name: "Balikis", arabic: "بَلْقِيسُ", meaning: "Beautiful, Wealthy Queen " },
+  { name: "Firdaws", arabic: "فِرْدَوْس", meaning: "Garden of Paradise " },
+  { name: "Firdaus", arabic: "فِرْدَوْس", meaning: "Garden of Paradise " },
 
   // --- "Servant of Allah" (Abd-) names ---
   { name: "Abdullah", arabic: "عَبْدُ اللّٰهِ", meaning: "Servant of Allah" },
